aac306a4524b7171ae89c0d07f8a51b3f43d3958
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2024-10-04 12:34:11 +0200

    bring back view policy and fix getPostByUser method

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,6 +10,19 @@ class UserPolicy
 {
 
     use HandlesAuthorization;
+    protected $userService;
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+    
+    /**
+     * Determine whether the user can view the informations linked to the user.
+     */
+    public function view($post, $user): bool
+    {
+        return $this->userService->checkIfCanAccessToResource($user->id) && $this->userService->isUserUnblocked($user->id); 
+    }
 
     /**
      * Determine whether the user can update the informations linked to the user profile.
